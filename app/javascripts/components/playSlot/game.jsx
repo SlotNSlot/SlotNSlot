e5454fefa23d98e1b9c41c900173de70f98261c3
parkkoon <lukaku0903@gmail.com>
2017-07-04 15:29:02 +0900

    Add stopSpin Method

--- app/javascripts/components/playSlot/game.jsx
+++ app/javascripts/components/playSlot/game.jsx
@@ -14,7 +14,13 @@ export default class SlotGame {
     this.renderer = null;
     this.stage = null;
     this.gameLoop = this.gameLoop.bind(this);
+    this.loopStart = this.loopStart.bind(this);
+    this.stopSpin = this.stopSpin.bind(this);
+    this.stopStart = this.stopStart.bind(this);
 
+    // spinStatus Array initializes to true
+    // true means spinning, false means stopped.
+    this.spinStatus = new Array(ENTIRE_REEL_COUNT).fill(true);
     // Set Entire Canvas Properties
     this.renderer = PIXI.autoDetectRenderer(940, 660, {
       view: canvasElement,
@@ -33,9 +39,9 @@ export default class SlotGame {
     const symbols = new Array(ALL_SYMBOL_COUNT);
     let reels = [];
 
-    for (let idx = 0; idx < ENTIRE_REEL_COUNT; idx++) {
+    for (let idx = 0; idx < ENTIRE_REEL_COUNT; idx += 1) {
       reels[idx] = [];
-      for (let j = 0; j < ITEM_PER_ENTIRE_REEL; j++) {
+      for (let j = 0; j < ITEM_PER_ENTIRE_REEL; j += 1) {
         reels[idx].push(Math.floor(Math.random() * ALL_SYMBOL_COUNT));
       }
     }
@@ -45,9 +51,9 @@ export default class SlotGame {
     this.reelGroup = new Array(ITEM_PER_ENTIRE_REEL).fill(1);
     this.reelGroup.forEach((reelItem, index) => {
       this.reelGroup[index] = new PIXI.Container();
-      this.reelGroup[index].vy = 80;
+      this.reelGroup[index].vy = 0;
     });
-
+    // this.reelGroup.forEach(() => (new PIXI.Container()));
     // Add the canvas to the HTML document
     PIXI.loader
       .add('assets/images/symbolsMap.json')
@@ -59,9 +65,9 @@ export default class SlotGame {
       .load(() => {
         // Symbols make and make reel sprites.
         const spritesNum = [9, 7, 6, 7, 6, 9, 24, 19, 23, 24, 24, 24, 24];
-        for (let i = 1; i <= ALL_SYMBOL_COUNT; i++) {
+        for (let i = 1; i <= ALL_SYMBOL_COUNT; i += 1) {
           const imglist = [];
-          for (let j = 0; j < spritesNum[i - 1]; j++) {
+          for (let j = 0; j < spritesNum[i - 1]; j += 1) {
             const val = j < ITEM_PER_ENTIRE_REEL ? `0${j}` : j;
             const frame = PIXI.Texture.fromFrame(`Symbol${i}_${val}.png`);
             imglist.push(frame);
@@ -71,8 +77,8 @@ export default class SlotGame {
 
         reels = reels.map(reel => reel.map(e => new PIXI.extras.AnimatedSprite(symbols[e])));
 
-        for (let reelNum = 0; reelNum < ENTIRE_REEL_COUNT; reelNum++) {
-          for (let idx = 0; idx < reels[reelNum].length; idx++) {
+        for (let reelNum = 0; reelNum < ENTIRE_REEL_COUNT; reelNum += 1) {
+          for (let idx = 0; idx < reels[reelNum].length; idx += 1) {
             const symbol = reels[reelNum][idx];
             symbol.width = 100;
             symbol.height = 100;
@@ -103,50 +109,75 @@ export default class SlotGame {
 
         const spinBtn = new PIXI.Graphics();
         spinBtn.beginFill(0x6495ed, 1);
-        // Btn.lineStyle(2, 0x6495ed, 1);
+        spinBtn.lineStyle(2, 0x000000, 1);
         spinBtn.drawRect(0, 0, 100, 50);
         spinBtn.endFill();
         spinBtn.x = 700;
         spinBtn.y = 525;
         spinBtn.interactive = true;
         spinBtn.buttonMode = true;
-        spinBtn.on('pointerdown', () => {
-          // alert('hi!');
-          this.reelGroup.forEach(reel => (reel.vy = 80));
-        });
+        spinBtn.on('pointerdown', this.loopStart);
         UIContainer.addChild(spinBtn);
 
-        this.stage.addChild(UIContainer);
-
         const stopBtn = new PIXI.Graphics();
-        stopBtn.beginFill(0x6495ed, 1);
-        stopBtn.lineStyle(2, 0x6495ed, 1);
+        stopBtn.beginFill(0xcc0000, 1);
+        stopBtn.lineStyle(2, 0x000000, 1);
         stopBtn.drawRect(0, 0, 100, 50);
         stopBtn.endFill();
         stopBtn.x = 800;
         stopBtn.y = 525;
         stopBtn.interactive = true;
         stopBtn.buttonMode = true;
+        stopBtn.on('pointerdown', () => {
+          this.stopSpin(ENTIRE_REEL_COUNT).then(v => {
+            console.log(v);
+          });
+        });
         UIContainer.addChild(stopBtn);
+
+        this.stage.addChild(UIContainer);
         // Tell the `renderer` to `render` the `stage`
         this.renderer.render(this.stage);
+        // When load function is ended, then start gameLoop
       });
-    // When load function is ended, then start gameLoop
     this.gameLoop();
   }
 
+  loopStart() {
+    this.reelGroup.forEach(reel => (reel.vy = 80));
+    this.spinStatus.fill(true);
+  }
+
   gameLoop() {
+    console.log('is gameLoop ing...');
     window.requestAnimationFrame(this.gameLoop);
-    this.reelGroup.forEach(reel => {
+    this.reelGroup.forEach((reel, index) => {
+      reel.y += reel.vy;
+
+      if (!this.spinStatus[Math.floor(index / 2)]) {
+        reel.vy = reel.vy > 0 ? reel.vy - 0.4 : 0;
+      }
       if (reel.y > reel.height) {
         reel.y -= reel.height * 2;
-      } else {
-        reel.vy = reel.vy > 0 ? reel.vy - 0.4 : 0;
-        reel.y += reel.vy;
       }
     });
     // Render the stage to see the animation
     this.renderer.render(this.stage);
+  }
+
+  async stopSpin(spinLength) {
+    for (let i = 0; i < spinLength; i += 1) {
+      await this.stopStart(i);
+    }
+  }
+
+  stopStart(reelNum) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        this.spinStatus[reelNum] = false;
+        resolve(reelNum);
+      }, 2000);
+    });
   }
 
   removeCurrentGame() {

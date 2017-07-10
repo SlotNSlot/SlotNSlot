5422f733de9ca476d89b79b975e06389ec14fd1a
fish-sushi <sushi.otoro@outlook.com>
2017-07-10 21:36:18 +0900

    Removed unneeded console & script && Changed wrong right Items position

--- app/javascripts/components/slot/layouts/header.jsx
+++ app/javascripts/components/slot/layouts/header.jsx
@@ -3,41 +3,27 @@ import { Link, NavLink } from 'react-router-dom';
 import Icon from '../../../icons';
 import styles from './header.scss';
 
-const Header = ({ rootState, location }) => {
-  console.log(location);
-  return (
-    <div className={styles.header}>
-      <div className={styles.headerContainer}>
-        <Link to="/" className={styles.logo}>
-          <Icon className={styles.logo} icon="SLOT_N_SLOT_LOGO" />
-        </Link>
-        <div className={styles.leftItems}>
-          <NavLink
-            to="/slot/play"
-            className={styles.navItem}
-            activeClassName={styles.navItemActive}
-            location={location}
-          >
-            PLAY
-          </NavLink>
-          <NavLink
-            to="/slot/make"
-            className={styles.navItem}
-            activeClassName={styles.navItemActive}
-            location={location}
-          >
-            MAKE
-          </NavLink>
+const Header = ({ rootState, location }) =>
+  <div className={styles.header}>
+    <div className={styles.headerContainer}>
+      <Link to="/" className={styles.logo}>
+        <Icon className={styles.logo} icon="SLOT_N_SLOT_LOGO" />
+      </Link>
+      <div className={styles.leftItems}>
+        <NavLink to="/slot/play" className={styles.navItem} activeClassName={styles.navItemActive} location={location}>
+          PLAY
+        </NavLink>
+        <NavLink to="/slot/make" className={styles.navItem} activeClassName={styles.navItemActive} location={location}>
+          MAKE
+        </NavLink>
+      </div>
+      <div className={styles.rightItems}>
+        <div className={styles.walletStatus}>
+          Your balance : {rootState.get('balance')} ETH
         </div>
-        <div className={styles.rightItems}>
-          <div className={styles.walletStatus}>
-            Your balance : {rootState.get('balance')} ETH
-          </div>
-          <button className={styles.accountBtn}>Your Account</button>
-        </div>
+        <button className={styles.accountBtn}>Your Account</button>
       </div>
     </div>
-  );
-};
+  </div>;
 
 export default Header;

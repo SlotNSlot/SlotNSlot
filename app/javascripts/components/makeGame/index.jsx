21b2be0b9bc0937f8af5e54b4a448193b21bac60
fish-sushi <sushi.otoro@outlook.com>
2017-07-12 02:04:54 +0900

    Made makegame page2

--- app/javascripts/components/makeGame/index.jsx
+++ app/javascripts/components/makeGame/index.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 // components
 import Icon from '../../icons';
 import MakeGameFirstStep from './firstStep';
+import MakeGameSecondStep from './secondStep';
 import MakeGameProgress from './progress';
 import Status404 from '../404';
 // styles
@@ -15,7 +16,16 @@ const MakeGame = ({ match }) => {
         <Icon className={styles.makeSlotIcon} icon="MAKE_SLOT" />
         <MakeGameProgress currentStep={parseInt(match.params.step, 10)} />
         <Switch>
-          <Route path={match.url} component={MakeGameFirstStep} />
+          <Route
+            path={match.url}
+            render={() => {
+              if (parseInt(match.params.step, 10) === 1) {
+                return <MakeGameFirstStep />;
+              } else if (parseInt(match.params.step, 10) === 2) {
+                return <MakeGameSecondStep />;
+              }
+            }}
+          />
           <Route component={Status404} />
         </Switch>
       </div>

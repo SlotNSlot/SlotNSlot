1d3dfecb5cbc0eab6df99ab0b0e1dcd00c56e074
c0smic0wl <c0smic0wl@outlook.com>
2017-07-12 23:52:59 +0900

    Make countdown function

--- app/javascripts/components/about/crowdSaleContainer/crowdSaleContainer.jsx
+++ app/javascripts/components/about/crowdSaleContainer/crowdSaleContainer.jsx
@@ -1,35 +1,81 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import styles from './crowdSaleContainer.scss';
 import Icon from '../../../icons';
+import { updateCountdown } from './actions';
 
-const crowdSaleContainer = () =>
-  <div className={styles.crowdSaleContainer}>
-    <div className={styles.ticketContainer}>
-      <Icon icon="CROWDSALE_TICKET" />
-      <div className={styles.ticketTitle}>
-        <strong>CROWDSALE</strong> Starts on <strong>August 15th</strong>, 2017
+function mapStateToProps(appState) {
+  return {
+    // pass
+    aboutCrowdSale: appState.aboutCrowdSale,
+  };
+}
+
+class CrowdSaleContainer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.updateTimer = this.updateTimer.bind(this);
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.updateTimer, 500);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { aboutCrowdSale } = this.props;
+    return (
+      <div className={styles.crowdSaleContainer}>
+        <div className={styles.ticketContainer}>
+          <Icon icon="CROWDSALE_TICKET" />
+          <div className={styles.ticketTitle}>
+            <strong>CROWDSALE</strong> Starts on <strong>August 15th</strong>, 2017
+          </div>
+          <div className={styles.timeCounter}>
+            <div className={styles.counterCell}>
+              <div className={styles.counterNumber}>
+                {aboutCrowdSale.get('days')}
+              </div>
+              <div className={styles.counterTag}>Days</div>
+            </div>
+            <div className={styles.timeColon}>:</div>
+            <div className={styles.counterCell}>
+              <div className={styles.counterNumber}>
+                {aboutCrowdSale.get('hours')}
+              </div>
+              <div className={styles.counterTag}>Hours</div>
+            </div>
+            <div className={styles.timeColon}>:</div>
+            <div className={styles.counterCell}>
+              <div className={styles.counterNumber}>
+                {aboutCrowdSale.get('minutes')}
+              </div>
+              <div className={styles.counterTag}>Minutes</div>
+            </div>
+            <div className={styles.timeColon}>:</div>
+            <div className={styles.counterCell}>
+              <div className={styles.counterNumber}>
+                {aboutCrowdSale.get('seconds')}
+              </div>
+              <div className={styles.counterTag}>Seconds</div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className={styles.timeCounter}>
-        <div className={styles.counterCell}>
-          <div className={styles.counterNumber}>34</div>
-          <div className={styles.counterTag}>Days</div>
-        </div>
-        <div className={styles.timeColon}>:</div>
-        <div className={styles.counterCell}>
-          <div className={styles.counterNumber}>21</div>
-          <div className={styles.counterTag}>Hours</div>
-        </div>
-        <div className={styles.timeColon}>:</div>
-        <div className={styles.counterCell}>
-          <div className={styles.counterNumber}>56</div>
-          <div className={styles.counterTag}>Minutes</div>
-        </div>
-        <div className={styles.timeColon}>:</div>
-        <div className={styles.counterCell}>
-          <div className={styles.counterNumber}>32</div>
-          <div className={styles.counterTag}>Seconds</div>
-        </div>
-      </div>
-    </div>
-  </div>;
-export default crowdSaleContainer;
+    );
+  }
+
+  updateTimer() {
+    const { dispatch } = this.props;
+    dispatch(updateCountdown());
+    // const dueDate = new Date('2017-08-15T14:00:00.000Z');
+    // this.props.due
+    // console.log('hi');
+    // console.log(this.props.aboutCrowdSale.get('dueDate'));
+  }
+}
+export default connect(mapStateToProps)(CrowdSaleContainer);
--- app/javascripts/rootReducer.jsx
+++ app/javascripts/rootReducer.jsx
@@ -3,16 +3,19 @@ import { routerReducer } from 'react-router-redux';
 import * as RootReducer from './root/reducer';
 import * as MakeGameReducer from './components/makeGame/reducer';
 import * as AboutLayoutReducer from './components/about/layouts/reducer';
+import * as AboutCrowdSaleReducer from './components/about/crowdSaleContainer/reducer';
 
 export const initialState = {
   root: RootReducer.initialState,
   makeGame: MakeGameReducer.initialState,
   aboutLayout: AboutLayoutReducer.initialState,
+  aboutCrowdSale: AboutCrowdSaleReducer.initialState,
 };
 
 export const rootReducer = Redux.combineReducers({
   root: RootReducer.reducer,
   makeGame: MakeGameReducer.reducer,
   aboutLayout: AboutLayoutReducer.reducer,
+  aboutCrowdSale: AboutCrowdSaleReducer.reducer,
   router: routerReducer,
 });

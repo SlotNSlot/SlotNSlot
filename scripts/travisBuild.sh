cdca8ff66886958b0cb67e47d06e43aa31e11237
fish-sushi <sushi.otoro@outlook.com>
2017-08-03 21:36:20 +0900

    Update travis deploy script (#139)

--- scripts/travisBuild.sh
+++ scripts/travisBuild.sh
@@ -5,7 +5,7 @@ echo "${TRAVIS_BRANCH}"
 echo "${TRAVIS_PULL_REQUEST_BRANCH}"
 echo "${TRAVIS_PULL_REQUEST}"
 
-if [ "${TRAVIS_PULL_REQUEST_BRANCH}" = "master" ] && ! [ "${TRAVIS_PULL_REQUEST}" ]; then
+if [ "${TRAVIS_BRANCH}" = "master" ] && ! [ "${TRAVIS_PULL_REQUEST}" ]; then
     npm run deploy:prod
 else
     npm run deploy:stage
